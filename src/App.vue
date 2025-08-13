df00cd6f9b1fc2851dc14909b6cb6d1bc977fcae
V <148882985+vVvOvOvVv@users.noreply.github.com>
2025-08-13 23:10:42 +0800

    Section Header
    - still needs the accent line
    - need to figure out how to give different section headers different router links

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,32 @@
     <header-bar />
     <br />
     <br />
-    <home-component />
-    <work-experience />
+    <div class="home container">
+      <home-component />
+    </div>
+    <div class="work container">
+      <section-header>Work & Experience</section-header>
+      <div class="work-list container d-flex flex-column align-items-center">
+        <work-experience />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import HeaderBar from './components/HeaderBar.vue'
 import HomeComponent from './components/Home.vue'
+import SectionHeader from './components/SectionHeader.vue'
 import WorkExperience from './components/WorkExperience.vue'
 
-import jsonData from './assets/data.json'
+// import jsonData from './assets/data.json'
 
 export default {
   name: 'App',
   components: {
     HeaderBar,
     HomeComponent,
+    SectionHeader,
     WorkExperience,
   },
 }
---
+++ src/components/SectionHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="section-header">
+    <colored-text colorType="tealType" size="45px" weight="bold"> <slot /> </colored-text>
+  </div>
+</template>
+
+<script>
+import ColoredText from './ColoredText.vue'
+
+export default {
+  name: 'SectionHeader',
+  components: {
+    ColoredText,
+  },
+}
+</script>
